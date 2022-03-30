918ccc8a6e3bd0a4b5c98766a3c69b49dd32cbe4
Sherry He <xsherryhe@gmail.com>
2022-03-29 23:47:47 -0400

    Refactored and revamped save system to improve flow and user options

--- hangman.rb
+++ hangman.rb
@@ -1,5 +1,4 @@
 require 'yaml'
-require_relative 'input_validation'
 
 module Hangman
   @min_length = 5
@@ -96,89 +95,145 @@ module Hangman
     end
   end
 
-  module SaveSystem
-    include InputValidation
-    # TODO: Implement a limit for number of save files and a way to delete save files
-    def save_dir
-      "#{File.dirname(__FILE__)}/saves"
-    end
+  module SaveLoadSystem
+    # TODO: Add a 'DELETE (Delete saved games)' option to main menu
+    module Shared
+      def save_dir
+        "#{File.dirname(__FILE__)}/saves"
+      end
 
-    def save_record
-      "#{File.dirname(__FILE__)}/save_record.txt"
-    end
+      def save_record
+        "#{File.dirname(__FILE__)}/save_record.txt"
+      end
 
-    def save_game
-      Dir.mkdir(save_dir) unless Dir.exist?(save_dir)
+      def valid_save_name(max_length)
+        name = gets.chomp
+        reg = Regexp.new("^\\w{1,#{max_length}}|go back$", true)
+        until reg =~ name
+          puts 'Please enter at least one character.' if name.empty?
+          puts "Please enter a string of at most #{max_length} characters." if name.length > max_length
+          puts 'Please enter a string consisting of letters/numbers only.' if /[^\w]/ =~ name
+          name = gets.chomp
+        end
+        return if name.downcase == 'go back'
 
-      name = save_name
-      update_save_record(name)
-      File.open(save_dir + "/#{name}.yaml", 'w') { |yaml_file| to_yaml(yaml_file) }
-      puts "Game \"#{name}\" successfully saved!"
+        name
+      end
 
-      offer_game_exit
-    end
+      def name_record_reg(name)
+        Regexp.new("^#{name} (.*)$", true)
+      end
 
-    def save_name
-      loop do
-        puts 'Please type a name for your save file (max 15 characters, letters and numbers only, no spaces).'
-        name = alphanumeric_input(15)
-        return name unless name_record_reg(name) =~ File.read(save_record)
+      def update_save_record(name, add)
+        new_record = File.readlines(save_record)
+        new_record.reject! { |prior_save| name_record_reg(name) =~ prior_save }
+        new_record << "#{name.downcase} (#{correct_letters_in_word})" if add
+        File.open(save_record, 'w') do |record|
+          new_record.each { |save| record.puts(save) }
+        end
+      end
 
-        puts 'You already have a saved game with this name. Do you want to overwrite your previous save? Y/N'
-        return name if /^yes|y$/i =~ gets.chomp
+      def display_saved_games
+        puts 'SAVED GAMES:'
+        puts "\r\n"
+        File.readlines(save_record).each do |save|
+          puts "  -#{save}"
+        end
+        puts "\r\n"
       end
     end
 
-    def name_record_reg(name)
-      Regexp.new("^#{name} (.*)$", true)
-    end
+    module Save
+      def save_game
+        Dir.mkdir(save_dir) unless Dir.exist?(save_dir)
+        return unless (name = save_name)
 
-    def update_save_record(name)
-      new_record = File.readlines(save_record)
-      new_record.reject! { |prior_save| name_record_reg(name) =~ prior_save }
-      new_record << "#{name.downcase} (#{correct_letters_in_word})"
-      File.open(save_record, 'w') do |record|
-        new_record.each { |save| record.puts(save) }
+        update_save_record(name, true)
+        File.open(save_dir + "/#{name}.yaml", 'w') { |yaml_file| to_yaml(yaml_file) }
+        puts "Game \"#{name}\" successfully saved!"
+
+        offer_game_exit
+      end
+
+      def save_name
+        Dir.glob("#{save_dir}/*").size < 20 ? new_save_name : overwrite_name
+      end
+
+      def new_save_name
+        loop do
+          puts 'Type "GO BACK" to resume your game without saving.'
+          puts 'Please type a name for your save file (max 15 characters, letters and numbers only, no spaces).'
+          return unless (name = valid_save_name(15))
+          return name unless name_record_reg(name) =~ File.read(save_record)
+
+          puts 'You already have a saved game with this name. Do you want to overwrite your previous save? Y/N'
+          return name if /^yes|y$/i =~ gets.chomp
+        end
+      end
+
+      def overwrite_name
+        display_saved_games
+        puts 'Your save files are full.'
+        loop do
+          puts 'Please type the name of one of the above existing save files to overwrite, ' \
+               'or type "GO BACK" to resume your game without saving.'
+          return unless (name = valid_save_name(15))
+
+          existing_save_game = name_record_reg(name) =~ File.read(save_record)
+          return name if existing_save_game && /^yes|y$/i =~ overwrite_name_confirm(name)
+          return if !existing_save_game && /^yes|y|go back$/i =~ overwrite_name_resume
+        end
+      end
+
+      def overwrite_name_confirm(name)
+        puts "Overwrite the save file \"#{name}\"? Y/N"
+        gets.chomp
+      end
+
+      def overwrite_name_resume
+        puts 'There is no save file with that name. Resume game without saving? Y/N'
+        gets.chomp
+      end
+
+      def offer_game_exit
+        puts 'Exit to main menu? Y/N'
+        @game_over = true if /^yes|y|exit$/i =~ gets.chomp
       end
     end
 
-    def offer_game_exit
-      puts 'Exit to main menu? Y/N'
-      @game_over = true if /^yes|y$/i =~ gets.chomp
-    end
+    module Load
+      def load_game
+        if !Dir.exist?(save_dir) || Dir.empty?(save_dir)
+          puts 'Sorry, you have no saved games. Press RETURN to exit to main menu.'
+          gets
+          return @game_over = true
+        end
 
-    def load_game
-      if !Dir.exist?(save_dir) || Dir.empty?(save_dir)
-        puts 'Sorry, you have no saved games. Press RETURN to exit to main menu.'
-        gets
-        return @game_over = true
+        display_saved_games
+        name = load_name
+        return @game_over = true unless name
+
+        from_yaml(save_dir + "/#{name}.yaml")
+        puts "Game \"#{name}\" successfully loaded!"
       end
 
-      display_save_files
-      name = load_name
-      return @game_over = true unless name
+      def load_name
+        loop do
+          puts 'Type "GO BACK" if you wish to exit to main menu.'
+          puts 'Please type the name of the game you wish to load.'
+          return unless (name = valid_save_name(15))
+          return name if name_record_reg(name) =~ File.read(save_record)
 
-      from_yaml(save_dir + "/#{name}.yaml")
-      puts "Game \"#{name}\" successfully loaded!"
-    end
-
-    def display_save_files
-      puts 'SAVED GAMES:'
-      puts "\r\n"
-      File.readlines(save_record).each do |save|
-        puts "  -#{save}"
+          puts 'There is no saved game with that name. Exit to main menu? Y/N'
+          return if /^yes|y|go back$/i =~ gets.chomp
+        end
       end
-      puts "\r\n"
     end
 
-    def load_name
-      loop do
-        puts 'Please type the name of the game you wish to load.'
-        name = alphanumeric_input(15)
-        return name if name_record_reg(name) =~ File.read(save_record)
-
-        puts 'There is no saved game with that name. Exit to main menu? Y/N'
-        return if /^yes|y$/i =~ gets.chomp
+    module Delete
+      def delete_game(name)
+        update_save_record(name, false)
+        File.delete(save_dir + "/#{name}.yaml")
       end
     end
   end
@@ -186,7 +241,10 @@ module Hangman
   class Game
     include Hangman::Visual
     include Hangman::GameInputValidation
-    include Hangman::SaveSystem
+    include Hangman::SaveLoadSystem::Shared
+    include Hangman::SaveLoadSystem::Save
+    include Hangman::SaveLoadSystem::Load
+    include Hangman::SaveLoadSystem::Delete
 
     def initialize(new_game)
       new_game ? start_new_game : load_game
